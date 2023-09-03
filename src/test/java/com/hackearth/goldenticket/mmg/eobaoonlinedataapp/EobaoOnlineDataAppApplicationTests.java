5d25828efa8e936f175b8e4f58c089265903aedc
muraliweb9 <muraliweb@hotmail.com>
2023-09-03 12:06:30 +0100

    Initial

---
+++ src/test/java/com/hackearth/goldenticket/mmg/eobaoonlinedataapp/EobaoOnlineDataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackearth.goldenticket.mmg.eobaoonlinedataapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EobaoOnlineDataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
